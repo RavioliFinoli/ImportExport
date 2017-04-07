d6489d7c2b5f6a229c01308119a2b45437713f21
Elias Finoli <elle_finoli@hotmail.com>
2017-04-07 14:04:00 +0200

    added structs to MeshHeader.h

--- ImportExport/FBXConverter/MeshHeader.h
+++ ImportExport/FBXConverter/MeshHeader.h
@@ -1,5 +1,26 @@
 #pragma once
+#include <stdint.h>
+#include <d3d11.h>
+#include <DirectXMath.h>
 struct MeshHeader
 {
-	uint_8
+	uint8_t meshNameLength;
+	//name
+
+	uint32_t numberOfVerts;
+	//verts
+
+	uint32_t numberOfIndex;
+	//index
+
+	uint8_t textureNameLength;
+	//tex name (texture.png)
+};
+
+struct Vertex
+{
+	float posX, posY, posZ;
+	float norX, norY, norZ;
+	float U, V;
+	//other stuff?
 };
