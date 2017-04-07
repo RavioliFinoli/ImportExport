05e20166b9c178e952ee9e01e771f8651c062130
Elias Finoli <elle_finoli@hotmail.com>
2017-04-07 14:04:49 +0200

    Implemented test of binary input/out in main.cpp

--- ImportExport/FBXConverter/main.cpp
+++ ImportExport/FBXConverter/main.cpp
@@ -1,7 +1,41 @@
 #include "FBXImportExport.h"
+#include <fstream>
+#include "MeshHeader.h"
+#include <iostream>
+#include <stdlib.h>
+#define out std::cout <<
+void PrintVertex(Vertex vertex)
+{
+	out "Position: { " << vertex.posX << ", " << vertex.posY << ", " << vertex.posZ << "}" << std::endl;
+	out "Normal: { " << vertex.norX << ", " << vertex.norY << ", " << vertex.norZ << "}" << std::endl;
+	out "UV: { " << vertex.U << ", " << vertex.V << "}" << std::endl;
 
-int main() {
-	FBXImportExport importer;
-	FBXImportExport exporter;
-	return 0;
+}
+
+int main() 
+{
+
+	std::ofstream outFile("test3.G6", std::ios::binary);
+
+	Vertex vertices[3]
+	{
+		{ 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f },
+		{ 21.0f, 2.0f, 33.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f },
+		{ 1.0f, 2.0f, 3.0f, 20.0f, 0.0f, 1.0f, 31.0f, 0.0f }
+	};
+	outFile.write(reinterpret_cast<char*>(vertices), sizeof(Vertex) * 3);
+	outFile.close();
+	Vertex inVerts[3];
+
+	std::ifstream in("test3.G6", std::ios::binary);
+	in.read(reinterpret_cast<char*>(inVerts), sizeof(Vertex) * 3);
+
+	for (int i = 0; i < 3; i++)
+	{
+		std::cout << "Vertex " << i + 1 << std::endl;
+		out inVerts[i].posX << std::endl;
+	}
+	system("pause");
+	in.close();
+
 }
