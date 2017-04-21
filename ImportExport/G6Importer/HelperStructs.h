4dc368e800f64a77f3d8600af98d4af6d58eaccf
Elias Finoli <elle_finoli@hotmail.com>
2017-04-21 15:36:13 +0200

    added HelperStructs.h

---
+++ ImportExport/G6Importer/HelperStructs.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <Windows.h>
+#include <vector>
+#include <DirectXMath.h>
+
+struct CtrlPoint
+{
+	DirectX::XMFLOAT3 mPosition;
+	std::vector<BlendingIndexWeightPair> mBlendingInfo;
+
+	CtrlPoint()
+	{
+		mBlendingInfo.reserve(4);
+	}
+};
+
+struct BlendingIndexWeightPair
+{
+	unsigned int mBlendingIndex;
+	double mBlendingWeight;
+
+	BlendingIndexWeightPair() :
+		mBlendingIndex(0),
+		mBlendingWeight(0)
+	{}
+};
